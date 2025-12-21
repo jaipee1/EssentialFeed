2c6ced9fd182176e0b909b2b9c29e61538ce9ec7
Jai Prakash Yadav <yadavjaiprakash18@gmail.com>
2025-12-21 08:47:27 +0530

    feat: add feet item model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Jai Prakash Yadav on 21/12/25.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
